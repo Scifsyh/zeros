44b3721730629c072ff524bd046166c65161ed0b
Scifsyh <vjlad9520@gmail.com>
2018-09-15 12:00:33 +0300

    Solved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var count = 0;
+  for (var i = 1; Math.pow(5, i) < number; i++) {
+    count += Math.floor(number / Math.pow(5, i));
+  }
+  return count;
 }
